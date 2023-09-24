5302f964108dde5a719e9819c291053677630287
StudentRoman <143340583+StudentRoman@users.noreply.github.com>
2023-09-24 17:05:45 +0300

    Task-2 and task-3 fixes for week-1

--- week-1/task-3/src/main/java/edu/penzgtu/oop/DodecahedronVolume.java
+++ week-1/task-3/src/main/java/edu/penzgtu/oop/DodecahedronVolume.java
@@ -1,23 +1,33 @@
 package edu.penzgtu.oop;
 
 import java.util.Scanner;
-import java.util.InputMismatchException;
 
 public class DodecahedronVolume {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        try {
-            System.out.print("Enter the edge length ");
+        System.out.print("Enter the edge length ");
+        String userInput = scanner.nextLine();
 
-            int edge = scanner.nextInt();
-            double volume = ((15 + 7 * Math.sqrt(5)) * Math.pow(edge, 3)) / 4;
-
-            System.out.printf("The volume of the dodecahedron = %s", volume);
-        } catch (InputMismatchException e) {
-            System.out.printf("Wrong input format: %s", e.getClass());
+        if (!isValidInput(userInput)) {
+            System.err.println("Wrong input format");
+            return;
         }
 
+        int edge = Integer.parseInt(userInput);
+        double volume = ((15 + 7 * Math.sqrt(5)) * Math.pow(edge, 3)) / 4;
+
+        System.out.printf("The volume of the dodecahedron = %s", volume);
+
         scanner.close();
+    }
+
+    private static boolean isValidInput(String input) {
+        try {
+            Integer.parseInt(input);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
